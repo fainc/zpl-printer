5ae6d560b351394fc792acaa7a61a40798d93eb8
PickMeUp <zx@lendata.net>
2024-01-20 22:52:14 +0800

    update printer status decode

--- util.go
+++ util.go
@@ -15,30 +15,31 @@ func String2UintPTR(s string) (ret uintptr) {
 	return
 }
 
+// StatusDecode 按位与操作后以新 int 值形式直接表示文档对应二进制值，方便理解和后续逻辑判断
 func StatusDecode(status uintptr) int {
-	if status == 0 {
+	if status == 0 { // 正常
 		return 0
 	}
-	if (status & 0b1) > 0 {
+	if (status & 0b1) > 0 { // 打印头被打开
 		return 1
 	}
-	if (status & 0b10) > 0 {
+	if (status & 0b10) > 0 { // 卡纸
 		return 2
 	}
-	if (status & 0b100) > 0 {
-		return 3
-	}
-	if (status & 0b1000) > 0 {
+	if (status & 0b100) > 0 { // 缺纸
 		return 4
 	}
-	if (status & 0b10000) > 0 {
-		return 5
+	if (status & 0b1000) > 0 { // 缺碳带
+		return 8
 	}
-	if (status & 0b100000) > 0 {
-		return 6
+	if (status & 0b10000) > 0 { // 打印暂停
+		return 16
 	}
-	if (status & 0b1000000) > 0 {
-		return 7
+	if (status & 0b100000) > 0 { // 打印中
+		return 32
 	}
-	return 8
+	if (status & 0b1000000) > 0 { // 上盖打开
+		return 64
+	}
+	return 128 // 其它错误
 }
